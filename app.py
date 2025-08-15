e2b74ebb24cccd778298cde75d36f6693a89a344
SabinaEnergy <sabinacoach.ru@gmail.com>
2025-08-16 02:41:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,27 @@
 import os
-from flask import Flask, render_template, send_from_directory
+import requests
+from flask import Flask, render_template
 
-# шаблоны лежат в webapp, статика в webapp/static, а у статики URL будет /static
 app = Flask(__name__, template_folder="webapp", static_folder="webapp/static", static_url_path="/static")
 
 @app.get("/")
 def health():
     return "OK"
 
-# страница /hd
 @app.get("/hd")
 def hd():
     return render_template("index.html")
 
-# (опционально) если хочешь раздавать любые файлы из webapp по /hd/...
-@app.get("/hd/<path:fname>")
-def hd_files(fname):
-    return send_from_directory("webapp", fname)
+# Маршрут для установки вебхука
+@app.get("/set-webhook")
+def set_webhook():
+    bot_token = os.getenv("BOT_TOKEN")
+    base_url = os.getenv("BASE_URL")
+    secret_token = os.getenv("SECRET_TOKEN")
+    url = f"https://api.telegram.org/bot{bot_token}/setWebhook"
+    webhook_url = f"{base_url}/webhook"
+    resp = requests.get(url, params={"url": webhook_url, "secret_token": secret_token})
+    return resp.json()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
