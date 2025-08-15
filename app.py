33f86767994b3b05b98b2a654d7db3e054066b61
SabinaEnergy <sabinacoach.ru@gmail.com>
2025-08-16 02:21:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder="webapp")
+app = Flask(__name__, static_folder='webapp/static', template_folder='webapp')
 
-@app.route("/")
-def home():
-    return "OK"
+@app.route('/hd')
+def hd():
+    return render_template('index.html')
 
-@app.route("/hd")
-def hd_form():
-    return render_template("index.html")
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
